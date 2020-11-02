c0d6c1627df173c0b9149a2b5ea902e15f2f722a
Drew Thompson <drewallynthompson@gmail.com>
2020-11-02 09:16:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/' do
+  post '/piglatinize' do
     @piglatinized = PigLatinizer.new.piglatinize(params[:user_phrase])
     
   end
