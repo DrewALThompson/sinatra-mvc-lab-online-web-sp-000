b1ab92841a3add5d23c969768805887c7f1f74e3
Drew Thompson <drewallynthompson@gmail.com>
2020-11-02 08:56:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end
+  
+  post '/' do
+    @pg_text = PigLatinizer.new(params[:user_text])
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,21 @@
+class PigLatinizer
+  
+  def pg_word(word)
+    word.split('')
+    if word[0] == (/aeiouAEIOU/)
+      word.push(way)
+    else
+      if word[0] == (/[^aeiouAEIOU]/) && word[1] == (/[^aeiouAEIOU]/)
+        append = word[0,1].join('') + 'ay'
+        word.slice(2, (word.length + 1)).push(append).join('')
+      else
+        word.push(word[0] + 'ay').shift.join('')
+      end
+    end
+    word
+  end
+  
+  def pg_sentence(text)
+    text.
+  end
+end
